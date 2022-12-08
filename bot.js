0131e4de2cdb6b33a316fe65723588fac255ed1d
Andrew <59783848+Excidious@users.noreply.github.com>
2022-12-08 01:56:30 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+  const user = newPresence.user;
+
+  // Check if the user is the one we want to ping
+  if (user.id === '256213318060998666') {
+    // Check if the user was offline and is now online
+    if (oldPresence.status === 'offline' && newPresence.status === 'online') {
+
+      // Get the channel to send the message in
+      const channel = client.channels.cache.get('349096556818857995');
+
+      // Send the message
+      channel.send(`<@&1019364021746552883> is now online!`);
+    }
+  }
+});
+
+client.login('TOKEN');
